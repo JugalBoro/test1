acd715e6c7517c495c4a3a7f9ccb8954f589ea69
Pri <jboro999@gmail.com>
2022-10-03 08:57:03 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
-    dialect: 'mysql',
-    DB_HOST: process.env.DB_host
+    dialect:'mysql',
+    host:process.env.DB_HOST
 })
 
 module.exports = sequelize;
